2985b6e92d481c05324e3c92bddd30e013202e08
dalhyun2002 <dalhyun2002@naver.com>
2025-01-22 20:48:42 +0900

    [Bronze II] Title: 상수, Time: 168 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -5,10 +5,10 @@ public class Main {
       Scanner sc=new Scanner(System.in);
       String[] s=sc.nextLine().split(" ");
 
-    String a=new StringBuilder().append(s[0]).reverse().toString();
-    String b=new StringBuilder().append(s[1]).reverse().toString();
+    int a=Integer.parseInt(new StringBuilder().append(s[0]).reverse().toString());
+    int b=Integer.parseInt(new StringBuilder().append(s[1]).reverse().toString());
 
-    System.out.println(Integer.parseInt(a)>Integer.parseInt(b)?a:b);
+    System.out.println(a>b?a:b);
 
     }
 }
