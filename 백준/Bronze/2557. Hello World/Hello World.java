0ef8218bde321ebdd6767cac27da7cef01777395
김달현 <dalhyun2002@naver.com>
2025-01-26 22:30:43 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13908 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,11 +1,10 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
-       BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-       
-       bw.write("Hello World!");
-       bw.flush();
-       bw.close();
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder();
+        sb.append("Hello World!");
+
+        System.out.print(sb);
     }
 }
