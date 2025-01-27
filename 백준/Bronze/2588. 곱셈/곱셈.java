cd7ca8e3ed982473c269781a6821440d04baf582
김달현 <dalhyun2002@naver.com>
2025-01-27 15:39:00 +0900

    [Bronze III] Title: 곱셈, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,14 +1,14 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        String b=sc.next();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int a=Integer.parseInt(br.readLine());
+        String b=br.readLine();
 
-        System.out.println(a*(b.charAt(2)-'0'));
-        System.out.println(a*(Integer.parseInt(String.valueOf(b.charAt(1)))));
-        System.out.println(a*(b.charAt(0)-'0'));
+        System.out.println(a*Integer.parseInt(String.valueOf(b.charAt(2))));
+        System.out.println(a*Integer.parseInt(String.valueOf(b.charAt(1))));
+        System.out.println(a*Integer.parseInt(String.valueOf(b.charAt(0))));
         System.out.println(a*Integer.parseInt(b));
     }
 }
